fbff8d23cacf8701eb386456f7b11d443921405d
Leo Dirac <leo@scaledrecognition.com>
2014-10-05 14:03:38 -0700

    Just loads word list up to 31 words.

---
+++ combos.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from sets import Set
+
+class Wordlist:
+
+    def __init__(self,fn):
+        self.words = Set()
+        self.load(fn, 31)
+
+    def load(self, fn, limit=999):
+        fh = open(fn)
+        for word in fh.readlines():
+            self.words.add(word.rstrip())
+            if len(self.words) == limit:
+                return
+
+    def show(self):
+        for word in self.words:
+            print word
+
+
+
+if __name__ == "__main__":
+    words = Wordlist('wordlist.txt')
+    words.show()
+
+        
+

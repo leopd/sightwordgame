38029b644904b9e64645bf33340c6def8ad0b6fc
Leo Dirac <leo@scaledrecognition.com>
2014-10-05 14:55:24 -0700

    Finds a valid solution by shuffling and looking for the biggest deck.

--- combos.py
+++ combos.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 
 import itertools
+import random
 from sets import Set
 import time
 
 
 class Wordlist:
 
-    def __init__(self,fn):
+    def __init__(self,fn, limit=999):
         self.words = Set()
-        self.load(fn, 31)
+        self.load(fn, limit)
 
-    def load(self, fn, limit=999):
+    def load(self, fn, limit):
         fh = open(fn)
         for word in fh.readlines():
             self.words.add(word.rstrip())
@@ -70,10 +71,14 @@ class Deck:
         return True
 
 
-    def find_card_pairs(self, words, words_per_card):
+    def find_card_pairs(self, words, words_per_card, shuffle=False):
         print "Finding all possible combos..."
         combos = subsets_of_size(words,words_per_card)
         print "Num of %d word combos: %d" % (words_per_card, len(combos))
+
+        if shuffle:
+            combos = list(combos)
+            random.shuffle(combos)
 
         progress = ProgressIndicator("deck size")
         for card in combos:
@@ -86,13 +91,46 @@ class Deck:
 
 
     def __repr__(self):
-        return "Deck size: %d\nDeck=%s" % (len(self._deck), str(self._deck))
+        out = "Deck size: %d\n" % len(self._deck)
+        for card in self._deck:
+            out += str(list(card))
+            out += "\n"
+        return out
+
+
+    def validate(self):
+        print "Validating deck"
+        pairs = subsets_of_size(self._deck, 2, True)
+        progress = ProgressIndicator("pairs checked",1)
+        for pair in pairs:
+            card1 = pair[0]
+            card2 = pair[1]
+            both = card1.intersection(card2)
+            if len(both) != 1:
+                print "!!! Bad card pair found with %s and %s" % (card1,card2)
+                return False
+            progress.update(0)
+        print "All good!"
+        return True
+
+
+    def size(self):
+        return len(self._deck)
 
 
 if __name__ == "__main__":
     words = Wordlist('wordlist.txt')
-    deck = Deck()
-    deck.find_card_pairs(words.words, 6)
-    print deck
+    print "Using %d words" % len(words.words)
+
+    best_deck = None
+    while True:
+        deck = Deck()
+        deck.find_card_pairs(words.words, 6, True)
+
+        if ((not best_deck) or (deck.size() > best_deck.size())) and deck.validate():
+            print "\nNew best!\n%s" % deck
+            best_deck = deck
+        else:
+            print "Worse.  Deck size=%d" % deck.size()
 
 

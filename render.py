be592d3323acaa78b0f080df89e30830c446ac4c
Leo Dirac <leo@scaledrecognition.com>
2014-10-05 18:46:26 -0700

    Option to render in colors to make finding matches easier.

--- render.py
+++ render.py
@@ -2,7 +2,12 @@
 """Renders deck.json file into postscript.
 
 Usage:
-    python render.py deck.json
+    python render.py deckfile.json num_colors
+
+e.g.
+    python reder.py deck5.json 3
+
+(Fewer colors is harder.)
 
 """
 
@@ -11,15 +16,16 @@ import sys
 import random
 
 
-class CompleteDeck:
+class DeckRenderer:
 
-    def __init__(self,fn):
+    def __init__(self,fn, max_colors):
         self.cards = json.load(open(fn))
         self.num_cards = len(self.cards)
         self.num_words = len(self.cards[0])
         self.cards_rendered = 0
         self.center_x = 0
         self.center_y = 0
+        self.max_colors = max_colors
         print """
             /Times-Roman findfont
             28 scalefont
@@ -39,6 +45,7 @@ class CompleteDeck:
         self.center_x = x * 250
         self.center_y = y * -250
         print """%d %d translate
+            0 0 0 setrgbcolor
             newpath
             0 0 100 0 360 arc
             stroke
@@ -47,13 +54,24 @@ class CompleteDeck:
 
 
     def render_word(self, word, position):
+        colors = [
+            '0 0 0',  # black
+            '1 0 0',  # red
+            '0 1 0',  # green
+            '0 0 1',  # blue
+            '1 0 1',  # purple
+            '0 1 1',  # cyan
+        ]
+        # pick a color deterministically to help make matching easier
+        color = colors[ hash(word) % self.max_colors ]
         print """
             %.0f rotate
             0 -80 translate
             %d 0 moveto
+            %s setrgbcolor
             (%s) show
             0 80 translate
-        """ % (360/self.num_words, len(word)*(-6), word)
+        """ % (360/self.num_words, len(word)*(-6), color, word.capitalize())
 
 
     def render_card(self,n):
@@ -74,21 +92,26 @@ class CompleteDeck:
 
 
 def ps_intro():
+    """Prints a postscript header
+    """
     print "%!\n"
 
 def ps_closer():
+    """Closes out the postscript
+    """
     print "\nshowpage\n"
 
 
 if __name__ == "__main__":
     try:
         fn = sys.argv[1]
+        colors = int(sys.argv[2])
     except:
         print __doc__
         sys.exit(-1)
 
-    deck = CompleteDeck(fn)
     ps_intro()
+    deck = DeckRenderer(fn, colors)
     for idx in range(deck.num_cards):
         deck.render_card(idx)
     ps_closer()
